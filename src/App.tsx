e0c7fda93aac8d8075b119173125733555053014
Daniel Avzaradel <daniel.avzaradel@gmail.com>
2024-01-20 03:25:24 +0200

    restart counter reducer app

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,8 @@
 import "./App.css";
-import Counter from "./features/counter/Counter";
 
 function App() {
   return (
     <>
-      <Counter />
     </>
   );
 }
--- src/main.tsx
+++ src/main.tsx
@@ -3,11 +3,10 @@ import ReactDOM from "react-dom/client";
 import App from "./App.tsx";
 import "./index.css";
 import { Provider } from "react-redux";
-import { store } from "./store/store.ts";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <Provider store={store}>
+    <Provider store={}>
       <App />
     </Provider>
   </React.StrictMode>
