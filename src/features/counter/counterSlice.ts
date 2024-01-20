4b1ddff7cf1fdcf74e2be23ea6727b923e8c2157
Daniel Avzaradel <daniel.avzaradel@gmail.com>
2024-01-20 03:41:39 +0200

    fixed type wtih redux actions

---
+++ src/features/counter/counterSlice.ts
@@ -0,0 +1,31 @@
+import { PayloadAction, createSlice } from "@reduxjs/toolkit";
+
+interface CounterState {
+    count: number
+}
+
+const initialState: CounterState = {
+    count: 0
+};
+
+export const counterSlice = createSlice({
+    name: 'counter',
+    initialState,
+    reducers: {
+        increment: (state: CounterState) => {
+            state.count += 1
+        }, 
+        decrement: (state: CounterState) => {
+            state.count -= 1
+        }, 
+        incrementByAmount: (state: CounterState, action: PayloadAction<number>) => {
+            state.count += action.payload
+        },
+        reset: (state: CounterState) => {
+            state.count = 0
+        },
+    }
+});
+
+export const { increment, decrement, incrementByAmount, reset } = counterSlice.actions;
+export default counterSlice.reducer
--- src/main.tsx
+++ src/main.tsx
@@ -3,10 +3,11 @@ import ReactDOM from "react-dom/client";
 import App from "./App.tsx";
 import "./index.css";
 import { Provider } from "react-redux";
+import { store } from "./assets/store/store.ts";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <Provider store={}>
+    <Provider store={store}>
       <App />
     </Provider>
   </React.StrictMode>
